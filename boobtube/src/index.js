2d012f7afe0442ad38106baccd0db1c380ffc797
esusslin <emmetsusslin@Emmets-MacBook-Air.local>
2018-11-07 16:34:35 -0800

    boobtube functional

--- boobtube/src/index.js
+++ boobtube/src/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
@@ -7,34 +8,43 @@ import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyAUzYCflraPNscmoxwOE6QrqRfdmMDmwsc";
 
-YTSearch(
-  {
-    key: API_KEY,
-    term: "surfboards"
-  },
-  data => console.log(data)
-);
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
+    this.videoSearch("disco biscuits");
+  }
+
+  videoSearch(term) {
     YTSearch(
       {
         key: API_KEY,
-        term: "surfboards"
+        term: term
       },
-      videos => this.setState({ videos })
+      videos =>
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        })
     );
   }
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
